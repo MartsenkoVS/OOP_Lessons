795bce267e5a4a773bae80e1a16c5560543c59d5
MartsenkoVS <martsenkovs@gmail.com>
2024-08-14 18:42:50 +0100

    update class Negative_Number_Exception

--- Lesson_10_Lite/classes/negative_number_error.py
+++ Lesson_10_Lite/classes/negative_number_error.py
@@ -1,8 +1,8 @@
 class Negative_Number_Exception(Exception):
     def __init__(self, number: int, message='Число отрицательное'):
-        super().__init__(message)
         self.number = number
         self.message = message
+        super().__init__(self.message)
         
     def __str__(self):
         return f'{self.message}: {self.number}'
